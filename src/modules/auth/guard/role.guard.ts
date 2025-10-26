174e370d168b6517434f0a524a011dba620d03df
Arlingga Cahya Ramdhana <arlingga02@gmail.com>
2025-10-26 13:33:59 +0700

    fix: parse exception

--- src/modules/auth/guard/role.guard.ts
+++ src/modules/auth/guard/role.guard.ts
@@ -1,14 +1,14 @@
-import { Role, RoleEnum } from '@app/common/entity';
-import { ParseExeption } from '@app/common/util/exceptions';
+import { Role, RoleEnum } from "@app/common/entity";
+import { ParseException } from "@app/common/util/exceptions";
 import {
   CanActivate,
   ExecutionContext,
   Injectable,
   Logger,
-} from '@nestjs/common';
-import { Reflector } from '@nestjs/core';
-import { Observable } from 'rxjs';
-import { ROLE_KEY } from '../../../constant';
+} from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { Observable } from "rxjs";
+import { ROLE_KEY } from "../../../constant";
 
 @Injectable()
 export class RoleGuard implements CanActivate {
@@ -18,14 +18,14 @@ export class RoleGuard implements CanActivate {
     this.logger = new Logger(RoleGuard.name);
   }
   canActivate(
-    context: ExecutionContext,
+    context: ExecutionContext
   ): boolean | Promise<boolean> | Observable<boolean> {
-    this.logger.log('Validate user role');
+    this.logger.log("Validate user role");
 
     try {
       const requiredRoles = this.reflector.getAllAndOverride<RoleEnum[]>(
         ROLE_KEY,
-        [context.getHandler(), context.getClass()],
+        [context.getHandler(), context.getClass()]
       );
 
       if (!requiredRoles.length) {
@@ -35,10 +35,10 @@ export class RoleGuard implements CanActivate {
       const { user } = context.switchToHttp().getRequest();
 
       return requiredRoles.some((role) =>
-        user?.role.some((r: Role) => r.name === role),
+        user?.role.some((r: Role) => r.name === role)
       );
     } catch (e) {
-      ParseExeption(e);
+      ParseException(e);
     }
   }
 }
