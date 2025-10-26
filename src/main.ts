d86f7699bf355b8b8baf8c5d210a95af787f594d
Arlingga Cahya Ramdhana <arlingga02@gmail.com>
2025-10-26 13:55:07 +0700

    fix: adapter fastify

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,17 @@ import cors from "@fastify/cors";
 import { Logger, ValidationPipe } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from "@nestjs/platform-fastify";
+import type { NestFastifyApplication } from "@nestjs/platform-fastify";
+import { FastifyAdapter } from "@nestjs/platform-fastify";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
+  const adapter = new FastifyAdapter();
+
+  const app = (await NestFactory.create(
     AppModule,
-    new FastifyAdapter({})
-  );
+    adapter as any
+  )) as unknown as NestFastifyApplication;
 
   // const swaggerConfig = new DocumentBuilder()
   //   .setTitle("taproost-service")
